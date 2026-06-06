764922754a774d922a9ba91ed31f9b1ea2b25640
PosztoHUN <zsolt.posztobanyi@gmail.com>
2026-06-06 18:14:11 +0200

    418 v6

--- main.py
+++ main.py
@@ -1117,13 +1117,12 @@ async def m41(ctx):
             "next_stop": next_stop,
             "speed": speed,
             "delay_min": delay_min,
-            "show_icon": uic[5:8] == "418",
-            "file": image_file
+            "show_icon": uic[5:8] == "418"
         })
 
     image_bytes = generate_m41_status_image(entries)
     image_file = discord.File(image_bytes, filename="m41_status.png")
-    await ctx.send(f"🚆 Aktív M41 mozdonyok ({len(entries)} jármű)")
+    await ctx.send(f"🚆 Aktív M41 mozdonyok ({len(entries)} jármű)", file=image_file)
         
 @bot.command()
 async def bvh(ctx):
